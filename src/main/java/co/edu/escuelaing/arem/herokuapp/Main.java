35fae3bdcc63d3f2953f14787bb701c40f7def8f
unknown <2110805@labinfo.is.escuelaing.edu.co>
2018-04-18 10:56:22 -0500

    First commit

---
+++ src/main/java/co/edu/escuelaing/arem/herokuapp/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.arem.herokuapp;
+
+/**
+ *
+ * @author 2110805
+ */
+public class Main {
+    
+}
