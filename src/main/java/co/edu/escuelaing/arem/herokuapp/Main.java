83f705999cc90e8c6d783df2ae3109bc2be0e70a
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-04-18 21:51:10 -0500

    Deployed on heroku, aws lambda not configured

--- src/main/java/co/edu/escuelaing/arem/herokuapp/Main.java
+++ src/main/java/co/edu/escuelaing/arem/herokuapp/Main.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.escuelaing.arem.herokuapp;
 
-/**
- *
- * @author 2110805
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@SpringBootApplication
 public class Main {
-    
+     public static final String LINK_API_GATEWAY_DEPLOY = "https://nebqnnjewe.execute-api.us-west-2.amazonaws.com/prod/bill?id=";
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @RequestMapping(value="/bill")
+    public ResponseEntity<?> getJsonBill(@RequestParam("id") int id) throws MalformedURLException{
+        
+        URL link = new URL(LINK_API_GATEWAY_DEPLOY + id);
+            String ans = "";
+            try (BufferedReader reader
+                    = new BufferedReader(new InputStreamReader(link.openStream()))) {
+                String inputLine = null;
+                while ((inputLine = reader.readLine()) != null) {
+                    ans += inputLine;
+                }
+            } catch (IOException x) {
+                System.err.println(x);
+            }
+        
+        return new ResponseEntity<>(ans,HttpStatus.OK);
+    }
+
+  
+
 }
